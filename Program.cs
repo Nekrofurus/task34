e0f2774a30c11d110916136ae522389a8d5370fa
VVCephei <124810683+Nekrofurus@users.noreply.github.com>
2023-03-13 17:17:56 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
-Console.WriteLine("Hello, World!");
 Console.Write("Введите длинну массива = ");
 int length = Convert.ToInt32(Console.ReadLine());
 int[] CreateRandomArray(int length)
